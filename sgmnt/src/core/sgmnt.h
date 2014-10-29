d7ae7fbfe0f5c51f5518ec5102b29ea1af5fce36
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2014-10-29 17:14:59 +0900

    毎回 header ファイルを書くの面倒なので一気読み出来る header を作成.

---
+++ sgmnt/src/core/sgmnt.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// --- sgmnt::events
+
+#include "Event.h"
+#include "EventDispatcher.h"
+
+#include "OscEvent.h"
+#include "TimeUtilEvent.h"
+
+// --- sgmnt::display
+
+#include "IDrawable.h"
+#include "DisplayNode.h"
+
+#include "CameraTexture.h"
+#include "FboTexture.h"
+#include "MovieTexture.h"
+#include "Texture.h"
+
+// --- sgmnt::io
+
+#include "SiAudioInput.h"
+#include "SiCaptureInput.h"
+#include "SiOscInput.h"
+
+// --- sgmnt::utils
+
+#include "Utils.h"
