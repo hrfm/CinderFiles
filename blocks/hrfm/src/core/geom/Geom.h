9ef10787d3e4adf3397d541872729372ea39790c
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2014-12-25 02:55:08 +0900

    bend の実装がおかしかったので修正

--- blocks/hrfm/src/core/geom/Geom.h
+++ blocks/hrfm/src/core/geom/Geom.h
@@ -8,25 +8,35 @@ namespace hrfm{ namespace geom{
     inline static ci::Vec3f bend( ci::Vec3f context, float pitch, float yaw ){
         
         float     c, s;
-        ci::Quatf rotate;
-        
         ci::Vec3f norm     = context.normalized();
         ci::Quatf ctxQuatf = Quatf( 0.0, norm.x, norm.y, norm.z );
         float     length   = context.length();
         
+        ci::Quatf rotate;
+        ci::Quatf reset( norm, Vec3f( 0.0, 1.0, 0.0 ).normalized() );
+        
+        if( 0 < reset.getAngle() ){
+            ctxQuatf = reset.inverse() * ctxQuatf * reset;
+        }
+        
         // 回転その1
-        c        = cos( pitch / 2.0 );
-        s        = sin( pitch / 2.0 );
-        rotate   = Quatf( c, norm.y * s, norm.x * s, norm.z * s );
-        ctxQuatf = rotate.inverted() * ctxQuatf * rotate;
+        c = cos( pitch / 2.0 );
+        s = sin( pitch / 2.0 );
+        rotate   = ci::Quatf( c, 1.0 * s, 0.0 * s, 0.0 * s );
+        ctxQuatf = rotate.inverse() * ctxQuatf * rotate;
         
         // 回転その2
-        c        = cos( yaw / 2.0 );
-        s        = sin( yaw / 2.0 );
-        rotate   = Quatf( c, norm.x * s, norm.y * s, norm.z * s );
-        ctxQuatf = rotate.inverted() * ctxQuatf * rotate;
+        c = cos( yaw / 2.0 );
+        s = sin( yaw / 2.0 );
+        rotate   = ci::Quatf( c, 0.0 * s, 1.0 * s, 0.0 * s );
+        ctxQuatf = rotate.inverse() * ctxQuatf * rotate;
         
-        return ctxQuatf.getAxis().normalized() * length;
+        if( 0 < reset.getAngle() ){
+            ctxQuatf = reset * ctxQuatf * reset.inverse();
+        }
+        
+        
+        return ctxQuatf.v * length;
         
     }
     
