41aad7b17485b9ee97915b8d6db3472e2658fbf0
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2014-12-30 02:20:07 +0900

    ドロネー処理を cpp ファイルに移動

--- blocks/hrfm/src/core/cv/Delaunay.h
+++ blocks/hrfm/src/core/cv/Delaunay.h
@@ -17,110 +17,23 @@ namespace hrfm{ namespace cv{
         
     public:
         
-        Delaunay( Vec2i size = Vec2i( 320, 240 ) ){
-            
-            // SIFTまたはSURFを使う場合はこれを呼び出す．
-            //::cv::initModule_nonfree();
-            this->_detector = ::cv::FeatureDetector::create("FAST");
-            
-            init( size );
-            
-        };
-        ~Delaunay(){
-            
-        };
+        Delaunay( Vec2i size = Vec2i( 320, 240 ) );
+        ~Delaunay();
         
-        void init( Vec2i size = Vec2i( 320, 240 ) ){
-            _size = size;
-            _subDiv2D.initDelaunay( ::cv::Rect(0,0,_size.x,_size.y) );
-        }
+        void init( Vec2i size = Vec2i( 320, 240 ) );
         
-        void insertPoint( ::cv::Point2f point ){
-            _subDiv2D.insert( point );
-        }
-        void insertPoint( vector<::cv::Point2f> points ){
-            _subDiv2D.insert( points );
-        }
+        void insertPoint( ::cv::Point2f point );
+        void insertPoint( vector<::cv::Point2f> points );
+        void insertPoint( ci::gl::Texture texture );
         
-        void insertPoint( ci::gl::Texture texture ){
-            
-            _img = ::cv::Mat( toOcv( texture, CV_8UC3 ) );
-            
-            // 特徴点抽出の実行
-            vector<::cv::KeyPoint> keypoint;
-            _detector->detect( _img, keypoint );
-            
-            vector<::cv::Point2f> points;
-            vector<::cv::KeyPoint>::iterator it = keypoint.begin(), it_end = keypoint.end();
-            for(; it!=it_end; ++it) {
-                points.push_back( it->pt );
-            }
-            
-            init( texture.getSize() );
-            this->insertPoint( points );
-            
-        }
+        void update();
         
-        void update(){
-            _subDiv2D.getTriangleList( _triangles );
-        };
-        
-        void draw(){
-            _draw( 1.0, 1.0 );
-        }
-        
-        void draw( Vec2i size ){
-            _draw( (float)size.x / (float)_size.x, (float)size.y / (float)_size.y );
-        }
+        void draw();
+        void draw( Vec2i size );
         
     private:
         
-        void _draw( float scaleX = 1.0, float scaleY = 1.0 ){
-            
-            glBegin(GL_TRIANGLES);
-            for(auto it = _triangles.begin(), end=_triangles.end(); it != end; ++it )
-            {
-                
-                ::cv::Vec6f &vec = *it;
-                
-                Vec2f p1( vec[0] * scaleX, vec[1] * scaleY );
-                Vec2f p2( vec[2] * scaleX, vec[3] * scaleY );
-                Vec2f p3( vec[4] * scaleX, vec[5] * scaleY );
-                
-                if( 100 < (p1-p2).length() || 100 < (p2-p3).length() || 100 < (p3-p1).length() ){
-                    continue;
-                }
-                
-                int gX = floor( (vec[0]+vec[2]+vec[4]) / 3.0 );
-                if( gX < 0 ){
-                    gX = 0;
-                }else if( _size.x <= gX ){
-                    gX = _size.x - 1;
-                }
-                
-                int gY = floor( (vec[1]+vec[3]+vec[5]) / 3.0 );
-                if( gY < 0 ){
-                    gY = 0;
-                }else if( _size.y <= gY ){
-                    gY = _size.y - 1;
-                }
-                
-                ::cv::Vec3b color = _img.at<::cv::Vec3b>(gY,gX);
-                
-                int B = color[0];
-                int G = color[1];
-                int R = color[2];
-                
-                ci::gl::color( ci::Color( R / 255.0, G / 255.0, B / 255.0 ) );
-                ci::gl::vertex( p1 );
-                ci::gl::vertex( p2 );
-                ci::gl::vertex( p3 );
-                
-            }
-            glEnd();
-            
-        }
-        
+        void _draw( float scaleX = 1.0, float scaleY = 1.0, float threshold = 0.0 );
         
         pthread_mutex_t _mutex;
         std::thread mThread;
