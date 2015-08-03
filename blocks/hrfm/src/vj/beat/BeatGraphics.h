27b04f3a72c81f8536d85d8283f7ccdcae19d7e2
KAWAKITA Hirofumi <hrfm0818@gmail.com>
2015-08-03 23:28:11 +0900

    no message

--- blocks/hrfm/src/vj/beat/BeatGraphics.h
+++ blocks/hrfm/src/vj/beat/BeatGraphics.h
@@ -4,20 +4,6 @@
 #include "FilterBase.h"
 #include "BeatPresetBase.h"
 
-#include "BeatSquare.h"
-
-#include "BeatFilterColor.h"
-#include "BeatFilterSplit.h"
-#include "BeatFilterTransField.h"
-#include "BeatFilterTransTunnel.h"
-#include "BeatFilterDot.h"
-#include "BeatFilterScroll.h"
-
-#include "BeatPresetFieldV.h"
-#include "BeatPresetPolar.h"
-#include "BeatPresetSplit.h"
-#include "BeatPresetTunnel.h"
-
 namespace hrfm{ namespace vj{
     
     class BeatGraphics : public hrfm::display::DisplayNode{
